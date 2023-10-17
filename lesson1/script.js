3d4df340a0881a678604a832c323ff87655e1140
Giorgi Bidzishvili <bidzishviligiorgi7@gmail.com>
2023-10-17 09:01:43 +0400

    lesson1 is done

---
+++ lesson1/script.js
@@ -0,0 +1,20 @@
+// 1.
+var firstName = prompt('Enter Your firstName');
+var surName = prompt('Enter Your surName');
+var age = prompt('Enter Your age');
+
+
+// 2.
+var firstName = 'Giorgi';
+var surName = 'Bidzishvili';
+var age = '20';
+var email = 'g_bidzishvili@cu.edu.ge';
+var sex = 'male';
+var text = `Hi, I'm ${firstName} ${surName}, ${age} Years old! my email:${email} and sex: ${sex}`;
+document.write(text);
+
+// 3.
+
+var fullName = prompt('Enter Your FullName: `name surname`');
+var h1 = `<h1>${fullName}</h1>`;
+document.write(h1);
