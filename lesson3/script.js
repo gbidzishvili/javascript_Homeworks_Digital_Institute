2b47c42b5b8cece493fb300ae445e12100a65d4e
Giorgi Bidzishvili <bidzishviligiorgi7@gmail.com>
2023-10-17 11:05:17 +0400

    lesson3 is done

---
+++ lesson3/script.js
@@ -0,0 +1,28 @@
+// 1.
+var equation = 'our equation: 2*X – 3*Y + Z = 0';
+alert('our equation: 2*X – 3*Y + Z = 0');
+var y = prompt('Enter Y ');
+var z = prompt('Enter Z ');
+var x = (3 * y - z) / 2;
+console.log(`x is: ${x}`);
+// 2.
+// var a = prompt(`Enter size of side a`);
+// var b = prompt(`Enter size of side b`);
+// var c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+// console.log(`Hypotenuse equals: ` + c);
+// 3.
+// var a = Number(prompt(`Enter size of side a`));
+// var b = Number(prompt(`Enter size of side b`));
+// var c = Number(prompt(`Enter size of side c`));
+// var perimeter = a + b + c;
+// console.log(`triangle perimeter is: ${perimeter}`);
+
+// 4.
+// alert('our equation: 2X^2 – 2X + 13 = 0;');
+// var sqrtD = Math.sqrt(Math.abs(2 * 2 - 4 * 2 * 13));
+// console.log(
+// 	`Discriminant is less than 0 that's why we have irrational solution`
+// );
+// var x1 = `(2-${sqrtD}i)/4`;
+// var x2 = `(2+${sqrtD}i)/4`;
+// console.log(`x1 = ${x1};`, `x2 = ${x2};`);
